ad32dc3fb9dd98f6626ef5c08a265f0b4433a623
lllmoseslll <mosespeter620@gmail.com>
2025-07-03 16:37:06 +0300

    ratelimit12

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,19 +1,19 @@
 import ratelimit from "../config/upstash.js";
 
+const ratelimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("my-rate-limit");
 
-const ratelimiter = async (req, res, next) => { 
-    try {
-        const { success } = await ratelimit.limit("my-rate-limit")
-        
-        if (!success) {
-            return res.status(429).json({ error: "Rate limit exceeded. Please try again later." });
-        }
-        next();
-    } catch (error) {
-        console.error("Error in rate limiter middleware:", error);
-        return res.status(500).json({ error: "Internal server error." });
-        
+    if (!success) {
+      return res
+        .status(429)
+        .json({ error: "Rate limit exceeded. Please try again later." });
     }
-}
+    next();
+  } catch (error) {
+    console.error("Error in rate limiter middleware:", error);
+    return res.status(500).json({ error: "Internal server error." });
+  }
+};
 
 export default ratelimiter;
